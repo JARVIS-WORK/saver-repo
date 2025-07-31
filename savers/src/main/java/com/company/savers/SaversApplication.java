7cc90d880f7c83c38957743e19299fbfc1b146be
JARVIS-WORK <mahapatraravinarayan@gmail.com>
2025-07-31 23:08:31 +0530

    Initial commit - added my local project

---
+++ savers/src/main/java/com/company/savers/SaversApplication.java
@@ -0,0 +1,13 @@
+package com.company.savers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaversApplication.class, args);
+	}
+
+}
---
+++ savers/src/test/java/com/company/savers/SaversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.savers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
